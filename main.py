70721de1d0549208bce54e53ab6bed7b383ea6b5
Pau Baguer <pauinki@gmail.com>
2023-11-04 19:17:05 +0100

    implemented pca

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from scipy.io import arff
 import numpy as np
 import pandas as pd
+import sys
 import scipy as sc
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from preprocessing import Preprocessing
+from sklearn.decomposition import PCA
+from pca import Pca
 import birch, kmeans
 from sklearn.feature_selection import SelectKBest, SelectPercentile, f_classif, chi2, mutual_info_classif
 
@@ -44,115 +47,168 @@ if __name__ == '__main__':
     #             SelectKBest           #
     #####################################
     
-    print('SelectKBest with ANOVA F-test')
-    k = 2  # Number of top features to select
-    selector_adult = SelectKBest(score_func=f_classif, k=k)
-    X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
-    selected_adult = selector_adult.get_support(indices=True)
-    print(f"Selected features for adult dataset: {selected_adult}")
-    
-    selector_vowel = SelectKBest(score_func=f_classif, k=k)
-    X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df) 
-    selected_vowel = selector_vowel.get_support(indices=True)
-    print(f"Selected features for vowel dataset: {selected_vowel}")
-    
-    selector_pen = SelectKBest(score_func=f_classif, k=k)
-    X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
-    selected_pem = selector_pen.get_support(indices=True)
-    print(f"Selected features for pen dataset: {selected_pem}")
-    
-    plt.figure(figsize=(10, 8))
-    plt.subplot(3, 1, 1)
-    plt.subplots_adjust(hspace=0.5) # Add this line to increase the distance between subplots
-    plt.scatter(X_adult[:, 0], X_adult[:, 1], c=preprocessing.pp_gs_adult_df)
-    plt.xlabel('Feature ' + str(selected_adult[0]))
-    plt.ylabel('Feature ' + str(selected_adult[1]))
-    plt.title('Feature importance (F-test) for adult dataset')
-    
-    plt.subplot(3, 1, 2)
-    plt.scatter(X_vowel[:, 0], X_vowel[:, 1], c=preprocessing.pp_gs_vowel_df)
-    plt.xlabel('Feature ' + str(selected_vowel[0]))
-    plt.ylabel('Feature ' + str(selected_vowel[1]))
-    plt.title('Feature importance (F-test) for vowel dataset')
-   
-    plt.subplot(3, 1, 3)
-    plt.scatter(X_pen[:, 0], X_pen[:, 1], c=preprocessing.pp_gs_pen_based_df)
-    plt.xlabel('Feature' + str(selected_pem[0]))
-    plt.ylabel('Feature' + str(selected_pem[1]))
-    plt.title('Feature importance (F-test) for pen dataset')
-    plt.show()
-    
-    print('\n Select k best features with chi-squared test')
-    selector_adult = SelectKBest(score_func=chi2, k=k)
-    X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
-    selected_adult = selector_adult.get_support(indices=True)
-    print(f"Selected features for adult dataset: {selected_adult}")
-    
-    selector_vowel = SelectKBest(score_func=chi2, k=k)
-    X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df)
-    selected_vowel = selector_vowel.get_support(indices=True)
-    print(f"Selected features for vowel dataset: {selected_vowel}")
-    
-    selector_pen = SelectKBest(score_func=chi2, k=k)
-    X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
-    selected_pen = selector_pen.get_support(indices=True)
-    print(f"Selected features for pen dataset: {selected_pen}")
-    
-    plt.figure(figsize=(10, 8))
-    plt.subplots_adjust(hspace=0.5) # Add this line to increase the distance between subplots
-    plt.subplot(3, 1, 1)
-    plt.scatter(X_adult[:, 0], X_adult[:, 1], c=preprocessing.pp_gs_adult_df)
-    plt.xlabel('Feature '  + str(selected_adult[0]))
-    plt.ylabel('Feature ' + str(selected_adult[1]))
-    plt.title('Feature importance (chi-squared test) for adult dataset')
+    # print('SelectKBest with ANOVA F-test')
+    # k = 2  # Number of top features to select
+    # selector_adult = SelectKBest(score_func=f_classif, k=k)
+    # X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
+    # selected_adult = selector_adult.get_support(indices=True)
+    # print(f"Selected features for adult dataset: {selected_adult}")
+    #
+    # selector_vowel = SelectKBest(score_func=f_classif, k=k)
+    # X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df)
+    # selected_vowel = selector_vowel.get_support(indices=True)
+    # print(f"Selected features for vowel dataset: {selected_vowel}")
+    #
+    # selector_pen = SelectKBest(score_func=f_classif, k=k)
+    # X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
+    # selected_pem = selector_pen.get_support(indices=True)
+    # print(f"Selected features for pen dataset: {selected_pem}")
+    #
+    # plt.figure(figsize=(10, 8))
+    # plt.subplot(3, 1, 1)
+    # plt.subplots_adjust(hspace=0.5) # Add this line to increase the distance between subplots
+    # plt.scatter(X_adult[:, 0], X_adult[:, 1], c=preprocessing.pp_gs_adult_df)
+    # plt.xlabel('Feature ' + str(selected_adult[0]))
+    # plt.ylabel('Feature ' + str(selected_adult[1]))
+    # plt.title('Feature importance (F-test) for adult dataset')
+    #
+    # plt.subplot(3, 1, 2)
+    # plt.scatter(X_vowel[:, 0], X_vowel[:, 1], c=preprocessing.pp_gs_vowel_df)
+    # plt.xlabel('Feature ' + str(selected_vowel[0]))
+    # plt.ylabel('Feature ' + str(selected_vowel[1]))
+    # plt.title('Feature importance (F-test) for vowel dataset')
+    #
+    # plt.subplot(3, 1, 3)
+    # plt.scatter(X_pen[:, 0], X_pen[:, 1], c=preprocessing.pp_gs_pen_based_df)
+    # plt.xlabel('Feature' + str(selected_pem[0]))
+    # plt.ylabel('Feature' + str(selected_pem[1]))
+    # plt.title('Feature importance (F-test) for pen dataset')
+    # plt.show()
+    #
+    # print('\n Select k best features with chi-squared test')
+    # selector_adult = SelectKBest(score_func=chi2, k=k)
+    # X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
+    # selected_adult = selector_adult.get_support(indices=True)
+    # print(f"Selected features for adult dataset: {selected_adult}")
+    #
+    # selector_vowel = SelectKBest(score_func=chi2, k=k)
+    # X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df)
+    # selected_vowel = selector_vowel.get_support(indices=True)
+    # print(f"Selected features for vowel dataset: {selected_vowel}")
+    #
+    # selector_pen = SelectKBest(score_func=chi2, k=k)
+    # X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
+    # selected_pen = selector_pen.get_support(indices=True)
+    # print(f"Selected features for pen dataset: {selected_pen}")
+    #
+    # plt.figure(figsize=(10, 8))
+    # plt.subplots_adjust(hspace=0.5) # Add this line to increase the distance between subplots
+    # plt.subplot(3, 1, 1)
+    # plt.scatter(X_adult[:, 0], X_adult[:, 1], c=preprocessing.pp_gs_adult_df)
+    # plt.xlabel('Feature '  + str(selected_adult[0]))
+    # plt.ylabel('Feature ' + str(selected_adult[1]))
+    # plt.title('Feature importance (chi-squared test) for adult dataset')
+    #
+    # plt.subplot(3, 1, 2)
+    # plt.scatter(X_vowel[:, 0], X_vowel[:, 1], c=preprocessing.pp_gs_vowel_df)
+    # plt.xlabel('Feature ' + str(selected_vowel[0]))
+    # plt.ylabel('Feature ' + str(selected_vowel[1]))
+    # plt.title('Feature importance (chi-squared test) for vowel dataset')
+    #
+    # plt.subplot(3, 1, 3)
+    # plt.scatter(X_pen[:, 0], X_pen[:, 1], c=preprocessing.pp_gs_pen_based_df)
+    # plt.xlabel('Feature ' + str(selected_pen[0]))
+    # plt.ylabel('Feature ' + str(selected_pen[1]))
+    # plt.title('Feature importance (chi-squared test) for pen dataset')
+    # plt.show()
+    #
+    # #####################################
+    # #           N best plots            #
+    # #####################################
+    #
+    # import itertools
+    #
+    # # Select the top 5 features for each dataset
+    # k=4
+    # selector_adult = SelectKBest(score_func=f_classif, k=k)
+    # X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
+    # selected_adult = selector_adult.get_support(indices=True)
+    #
+    # selector_vowel = SelectKBest(score_func=f_classif, k=k)
+    # X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df)
+    # selected_vowel = selector_vowel.get_support(indices=True)
+    #
+    # selector_pen = SelectKBest(score_func=f_classif, k=k)
+    # X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
+    # selected_pen = selector_pen.get_support(indices=True)
+    #
+    # # Create a grid of plots for each pair of features
+    # datasets = [(X_adult, preprocessing.pp_gs_adult_df, selected_adult, 'adult'),
+    #             (X_vowel, preprocessing.pp_gs_vowel_df, selected_vowel, 'vowel'),
+    #             (X_pen, preprocessing.pp_gs_pen_based_df, selected_pen, 'pen')]
+    #
+    # for X, y, selected, dataset_name in datasets:
+    #     plt.figure(figsize=(10, 8))
+    #     plt.subplots_adjust(hspace=0.8, wspace=0.8) # Add this line to increase the distance between subplots
+    #     n=1
+    #     for i, j in itertools.combinations(range(X.shape[1]), 2):
+    #         plt.subplot(3, 2, n)
+    #         plt.scatter(X[:, i], X[:, j], c=y)
+    #         plt.xlabel('Feature ' + str(selected[i]))
+    #         plt.ylabel('Feature ' + str(selected[j]))
+    #         plt.title(f'{dataset_name} dataset: Features {selected[i]} vs {selected[j]}', fontsize=10)
+    #         n+=1
+    #     plt.show()
 
-    plt.subplot(3, 1, 2)
-    plt.scatter(X_vowel[:, 0], X_vowel[:, 1], c=preprocessing.pp_gs_vowel_df)
-    plt.xlabel('Feature ' + str(selected_vowel[0]))
-    plt.ylabel('Feature ' + str(selected_vowel[1]))
-    plt.title('Feature importance (chi-squared test) for vowel dataset')
-    
-    plt.subplot(3, 1, 3)
-    plt.scatter(X_pen[:, 0], X_pen[:, 1], c=preprocessing.pp_gs_pen_based_df)
-    plt.xlabel('Feature ' + str(selected_pen[0]))
-    plt.ylabel('Feature ' + str(selected_pen[1]))
-    plt.title('Feature importance (chi-squared test) for pen dataset')
-    plt.show()
-    
+
     #####################################
-    #           N best plots            #
+    #                PCA                #
     #####################################
-    
-    import itertools
 
-    # Select the top 5 features for each dataset
-    k=4
-    selector_adult = SelectKBest(score_func=f_classif, k=k)
-    X_adult = selector_adult.fit_transform(preprocessing.pp_adult_df, preprocessing.pp_gs_adult_df)
-    selected_adult = selector_adult.get_support(indices=True)
+    # TODO: select k for eigenvalues > 1
+    # TODO: ASK TEACHER FOR DECIMAL PROBLEM
+    # np.set_printoptions(threshold=sys.maxsize)
+    # adult_pca = Pca(preprocessing.pp_adult_df, 'Adult', 5)
+    # vowel_pca = Pca(preprocessing.pp_vowel_df, 'Vowel', 5)
+    #pen_based_pca = Pca(preprocessing.pp_pen_based_df, 'Pen based', 5)
 
-    selector_vowel = SelectKBest(score_func=f_classif, k=k)
-    X_vowel = selector_vowel.fit_transform(preprocessing.pp_vowel_df, preprocessing.pp_gs_vowel_df)
-    selected_vowel = selector_vowel.get_support(indices=True)
+    # test_arr2 = np.array([[9.0,39.0],
+    #                          [15.0,56.0],
+    #                          [25.0,93.0],
+    #                          [14.0,61.0],
+    #                          [10.0,50.0],
+    #                          [18.0,75.0],
+    #                          [0.0,32.0],
+    #                          [16.0,85.0],
+    #                          [5.0,42.0],
+    #                          [19.0,70.0],
+    #                          [16.0,66.0],
+    #                          [20.0,80.0]
+    #                          ])
 
-    selector_pen = SelectKBest(score_func=f_classif, k=k)
-    X_pen = selector_pen.fit_transform(preprocessing.pp_pen_based_df, preprocessing.pp_gs_pen_based_df)
-    selected_pen = selector_pen.get_support(indices=True)
+    test_arr2 = np.array([
+        [2.5,2.4],
+        [0.5,0.7],
+        [2.2,2.9],
+        [1.9,2.2],
+        [3.1,3.0],
+        [2.3,2.7],
+        [2.0,1.6],
+        [1.0,1.1],
+        [1.5,1.6],
+        [1.1,0.9]
+    ])
 
-    # Create a grid of plots for each pair of features
-    datasets = [(X_adult, preprocessing.pp_gs_adult_df, selected_adult, 'adult'),
-                (X_vowel, preprocessing.pp_gs_vowel_df, selected_vowel, 'vowel'),
-                (X_pen, preprocessing.pp_gs_pen_based_df, selected_pen, 'pen')]
+    print("mean:")
+    print(test_arr2.mean(axis=0))
+    print("var:")
+    print(test_arr2.var(axis=0))
 
-    for X, y, selected, dataset_name in datasets:
-        plt.figure(figsize=(10, 8))
-        plt.subplots_adjust(hspace=0.8, wspace=0.8) # Add this line to increase the distance between subplots
-        n=1
-        for i, j in itertools.combinations(range(X.shape[1]), 2):
-            plt.subplot(3, 2, n)
-            plt.scatter(X[:, i], X[:, j], c=y)
-            plt.xlabel('Feature ' + str(selected[i]))
-            plt.ylabel('Feature ' + str(selected[j]))
-            plt.title(f'{dataset_name} dataset: Features {selected[i]} vs {selected[j]}', fontsize=10)
-            n+=1
-        plt.show()
+    test_pca = Pca(test_arr2, 'test', 2)
+
+    print()
+    print("SKLEARN")
+    pca = PCA(n_components=2)
+    pca.fit(test_arr2)
+    print("Covariance matrix")
+    print(pca.get_covariance())
---
+++ pca.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+class Pca:
+    def __init__(self, original, dataset_name, k):
+        self.original_values = original
+        self.dataset_name = dataset_name
+        self.k = k # The K largest eigenvalues will be selected.
+        self.shape = self.original_values.shape
+        self.mean_vector = self.d_dimensional_mean_vector()
+        self.covariance_matrix = self.comp_covariance_matrix()
+        self.eigenvalues, self.eigenvectors = self.comp_eigenvectors()
+        self.k_eigenvectors_matrix = self.k_eigenvalues()
+        self.data_adjust = self.comp_data_adjust()
+        self.rotated_values = self.rotate_space()
+
+
+    def d_dimensional_mean_vector(self):
+        return self.original_values.mean(axis=0)
+
+    def covariance(self, A, mean_A, B, mean_B):
+        dist_A = A - mean_A
+        dist_B = B - mean_B
+
+        covar = np.sum(np.multiply(dist_A , dist_B)) / len(dist_A)
+        return covar
+
+    def comp_covariance_matrix(self):
+        c_matrix = np.zeros((self.shape[1], self.shape[1]))
+
+        for row_idx , _ in enumerate(c_matrix):
+            for col_idx, _ in enumerate(c_matrix.T):
+                co_var = self.covariance(self.original_values[:, row_idx], self.mean_vector[row_idx], self.original_values[:, col_idx], self.mean_vector[col_idx])
+                c_matrix.itemset((row_idx, col_idx), co_var)
+
+        print()
+        print(f"== {self.dataset_name} COVARIANCE MATRIX ==")
+        print(c_matrix)
+        print()
+        return c_matrix
+
+    def comp_eigenvectors(self):
+        eigenvalues, eigenvectors = np.linalg.eig(self.covariance_matrix)
+
+        eigenvectors = np.array([x for _, x in sorted(zip(eigenvalues, eigenvectors), reverse=True)])
+        eigenvalues = np.array([x for x in sorted(eigenvalues, reverse=True)])
+        eigen_df = pd.DataFrame(
+            {'Eigen values': eigenvalues, 'Eigen vectors': [str(v) for v in eigenvectors]})
+        # eigen_df.sort_values('Eigen values', ascending=False, inplace=True)
+
+        pd.set_option('display.max_colwidth', 10000)
+        print(eigen_df)
+        return eigenvalues, eigenvectors
+
+    def k_eigenvalues(self):
+        k_eigenvectors = self.eigenvectors[0:self.k]
+        return k_eigenvectors.T
+
+
+    def comp_data_adjust(self):
+        data_adjust_T = np.array([col - self.mean_vector[i] for i, col in enumerate(self.original_values.T)])
+        return data_adjust_T.T
+    def rotate_space(self):
+        transformed = np.matmul(self.k_eigenvectors_matrix.T, self.data_adjust.T)
+        return transformed
